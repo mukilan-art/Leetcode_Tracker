b6ea298b84b6c35c85cb4acbdac52c8fe67d8670
mukilan-art <mukilanmuthiah15@gmail.com>
2026-07-09 09:47:16 +0530

    Create 169-MajorityElement

---
+++ 169-MajorityElement/169-MajorityElement.java
@@ -0,0 +1,22 @@
+// Last updated: 7/9/2026, 9:47:16 AM
+class Solution {
+    public int majorityElement(int[] nums) {
+        int n=nums.length;
+        int cnt=0;
+        int el=0;
+        for(int i=0;i<n;i++){
+            if(cnt==0){
+                cnt=1;
+                el=nums[i];
+            }
+            else if(nums[i]==el){
+                cnt++;
+            }
+            else{
+                cnt--;
+            }
+        }
+        return el;
+        
+    }
+}

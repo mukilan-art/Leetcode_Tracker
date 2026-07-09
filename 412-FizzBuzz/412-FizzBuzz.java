2dd312426fa1eb060c28884850a23d2a8ee219a0
mukilan-art <mukilanmuthiah15@gmail.com>
2026-07-09 09:46:42 +0530

    Create 412-FizzBuzz

---
+++ 412-FizzBuzz/412-FizzBuzz.java
@@ -0,0 +1,19 @@
+// Last updated: 7/9/2026, 9:46:42 AM
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> ans = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 15 == 0) {
+                ans.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                ans.add("Fizz");
+            } else if (i % 5 == 0) {
+                ans.add("Buzz");
+            } else {
+                ans.add(String.valueOf(i));
+            }
+        }
+                         
+        return ans;                 
+    }
+}

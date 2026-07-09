ee41998b072c2895b8ebcc417b9f6936307ec0f8
mukilan-art <mukilanmuthiah15@gmail.com>
2026-07-09 09:46:07 +0530

    Create 941-SortArrayByParity

---
+++ 941-SortArrayByParity/941-SortArrayByParity.java
@@ -0,0 +1,14 @@
+// Last updated: 7/9/2026, 9:46:07 AM
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        int j=0;
+       for(int i =0;i<nums.length;i++){
+            if(nums[i]%2==0){
+                int temp =nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+                j++;
+            }
+        }
+    return nums;}
+}

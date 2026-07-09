0deb4eab318b1cc591fe58816f5fa93158d6478a
mukilan-art <mukilanmuthiah15@gmail.com>
2026-07-09 09:46:56 +0530

    Create 219-ContainsDuplicateIi

---
+++ 219-ContainsDuplicateIi/219-ContainsDuplicateIi.java
@@ -0,0 +1,16 @@
+// Last updated: 7/9/2026, 9:46:56 AM
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> seen = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int val = nums[i];
+            if (seen.containsKey(val) && i - seen.get(val) <= k) {
+                return true;
+            }
+            seen.put(val, i);
+        }
+
+        return false;        
+    }
+}

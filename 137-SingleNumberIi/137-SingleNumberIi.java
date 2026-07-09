11aeba2a60f376a54dd348d5a11002e208e43350
mukilan-art <mukilanmuthiah15@gmail.com>
2026-07-09 09:47:20 +0530

    Create 137-SingleNumberIi

---
+++ 137-SingleNumberIi/137-SingleNumberIi.java
@@ -0,0 +1,23 @@
+// Last updated: 7/9/2026, 9:47:20 AM
+class Solution {
+    public int singleNumber(int[] nums) {
+        for(int i=0;i<nums.length;i++)
+        {
+            int count=0;
+            for(int j=0;j<nums.length;j++)
+            {
+                if(nums[i]==nums[j])
+                {
+                    count++;
+                }
+            }
+                if(count==1)
+                {
+                    return nums[i];
+                }
+    
+            }
+              return -1;
+        }
+      
+    }

210b06ecab89a6eb7f530e931f798312e40fd49e
mukilan-art <mukilanmuthiah15@gmail.com>
2026-07-09 09:47:35 +0530

    Create 50-PowxN

---
+++ 50-PowxN/50-PowxN.java
@@ -0,0 +1,22 @@
+// Last updated: 7/9/2026, 9:47:34 AM
+class Solution {
+    public double myPow(double x, int n) {
+        return binaryExp(x, (long) n);
+    }
+
+    private double binaryExp(double x, long n) {
+        if (n == 0) {
+            return 1;
+        }
+       
+        if (n < 0) {
+            return 1.0 / binaryExp(x, -n);
+        }
+       
+        if (n % 2 == 1) {
+            return x * binaryExp(x * x, (n - 1) / 2);
+        } else {
+            return binaryExp(x * x, n / 2);
+        }
+    }
+}

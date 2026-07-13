7ed259edd6ccb0dbe433ffe7a19bb027c8829d43
mukilan-art <mukilanmuthiah15@gmail.com>
2026-07-13 13:35:32 +0530

    Create 3046-SplittheArray

---
+++ 3046-SplittheArray/version/java/3046-SplittheArray_20260713_133531.java
@@ -0,0 +1,21 @@
+// Last updated: 7/13/2026, 1:35:31 PM
+1class Solution {
+2    public boolean isPossibleToSplit(int[] nums) {
+3        for(int i=0;i<nums.length;i++)
+4        {
+5            int count=0;
+6            for(int j=0;j<nums.length;j++)
+7            {
+8                if(nums[i]==nums[j])
+9                {
+10                    count++;
+11                }
+12            }
+13            if(count>2)
+14            {
+15                return false;
+16            }
+17        }
+18        return true;
+19    }
+20}

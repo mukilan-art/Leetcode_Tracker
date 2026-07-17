406b059b326d6ca6c2e86ca336606f2bc1a2f398
mukilan-art <mukilanmuthiah15@gmail.com>
2026-07-17 08:51:19 +0530

    Create 2634-MinimumCommonValue

---
+++ 2634-MinimumCommonValue/2634-MinimumCommonValue.java
@@ -0,0 +1,24 @@
+// Last updated: 7/17/2026, 8:51:18 AM
+class Solution {
+    public int getCommon(int[] nums1, int[] nums2) {
+
+        int p1 = 0, p2 = 0;
+
+        while (p1 < nums1.length && p2 < nums2.length) {
+
+            if (nums1[p1] == nums2[p2]) {
+                return nums1[p1];
+            }
+
+            else if (nums1[p1] < nums2[p2]) {
+                p1++;
+            }
+
+            else {
+                p2++;
+            }
+        }
+
+        return -1;
+    }
+}

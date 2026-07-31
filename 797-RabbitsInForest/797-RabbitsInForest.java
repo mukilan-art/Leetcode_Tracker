f20efaa2c46acd3b66b4fbe43c950e4e5ca91365
mukilan-art <mukilanmuthiah15@gmail.com>
2026-07-31 09:31:06 +0530

    Create 797-RabbitsInForest

---
+++ 797-RabbitsInForest/797-RabbitsInForest.java
@@ -0,0 +1,19 @@
+// Last updated: 7/31/2026, 9:31:06 AM
+import java.util.Arrays;
+class Solution {
+    public int numRabbits(int[] answers) {
+        Arrays.sort(answers);
+        int res = 0, count = 0;
+        for (int i = 0; i < answers.length; i++) {
+            if (answers[i] == 0) {
+                res++;  
+            } else if (i == 0 || answers[i] != answers[i - 1] || count == 0) {
+                res += answers[i] + 1;  
+                count = answers[i];
+            } else {
+                count--;  
+            }
+        }
+        return res;
+    }
+}

2d346cc2e050a91e01650bc5e65b4d66e8ee810b
mukilan-art <mukilanmuthiah15@gmail.com>
2026-07-31 15:59:01 +0530

    Create 540-SingleElementInASortedArray

---
+++ 540-SingleElementInASortedArray/540-SingleElementInASortedArray.java
@@ -0,0 +1,19 @@
+// Last updated: 7/31/2026, 3:59:00 PM
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int low=0;
+        int high=nums.length - 1;
+        while (low<high) {
+            int mid=low+(high-low)/2;
+            if (mid%2==1){
+                mid--;
+            }
+            if (nums[mid]==nums[mid+1]) {
+                low=mid+2;
+            } else {
+                high=mid;
+            }
+        }
+        return nums[low];
+    }
+}

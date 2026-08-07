4affd79fddc7810139dfb7cb554568534d4242c8
mukilan-art <mukilanmuthiah15@gmail.com>
2026-08-07 15:53:43 +0530

    Create 881-BoatstoSavePeople

---
+++ 881-BoatstoSavePeople/version/java/881-BoatstoSavePeople_20260807_155343.java
@@ -0,0 +1,22 @@
+// Last updated: 8/7/2026, 3:53:43 PM
+1class Solution {
+2    public int numRescueBoats(int[] people, int limit) {
+3        int boatCount = 0;
+4        Arrays.sort(people);
+5        int left = 0;
+6        int right = people.length - 1;
+7        while(left <= right){
+8            int sum = people[left] + people[right];
+9            if(sum <= limit){
+10                boatCount++;
+11                left++;
+12                right--;
+13            }
+14            else{
+15                boatCount++;
+16                right--;
+17            }
+18        }
+19        return boatCount;
+20    }
+21}

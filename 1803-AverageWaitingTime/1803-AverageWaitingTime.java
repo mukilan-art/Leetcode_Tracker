37adc767295a146277a45b1a1ba772df85280ad9
mukilan-art <mukilanmuthiah15@gmail.com>
2026-08-14 12:37:56 +0530

    Create 1803-AverageWaitingTime

---
+++ 1803-AverageWaitingTime/1803-AverageWaitingTime.java
@@ -0,0 +1,17 @@
+// Last updated: 8/14/2026, 12:37:55 PM
+class Solution {
+    public double averageWaitingTime(int[][] customers) {
+        int n = customers.length, curTime = 0;
+        double totalTime = 0;
+        for(int i = 0; i < n; i++) {
+            if(curTime <= customers[i][0]) {
+                totalTime += customers[i][1];
+                curTime = customers[i][0] + customers[i][1];
+            } else {
+                totalTime += (curTime - customers[i][0] + customers[i][1]);
+                curTime += customers[i][1];
+            }
+        }
+        return totalTime / n;
+    }
+}

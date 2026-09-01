f43e290d657e94e5903a0c80b692dfb0f574afa1
mukilan-art <mukilanmuthiah15@gmail.com>
2026-09-01 16:18:39 +0530

    Create 389-FindTheDifference

---
+++ 389-FindTheDifference/389-FindTheDifference.java
@@ -0,0 +1,11 @@
+// Last updated: 9/1/2026, 4:18:39 PM
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int a=0,b=0;
+        for(int i=0;i<s.length();i++)
+            a+=s.charAt(i);
+        for(int i=0;i<t.length();i++)
+            b+=t.charAt(i);
+        return (char)(b-a);
+    }
+}

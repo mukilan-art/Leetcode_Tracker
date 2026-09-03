59f479e09a3adda9bf941edf2ad923a93e2f1859
mukilan-art <mukilanmuthiah15@gmail.com>
2026-09-03 16:29:43 +0530

    Create 451-SortCharactersByFrequency

---
+++ 451-SortCharactersByFrequency/451-SortCharactersByFrequency.java
@@ -0,0 +1,24 @@
+// Last updated: 9/3/2026, 4:29:39 PM
+class Solution {
+    public String frequencySort(String s) {
+        StringBuilder sb = new StringBuilder();
+        int[] freq = new int[128];
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            freq[s.charAt(i)]++;
+        }
+        while (sb.length() < n) {
+            int max = 0;
+            for (int i = 1; i < 128; i++) {
+                if (freq[i] > freq[max]) {
+                    max = i;
+                }
+            }
+            for (int i = 0; i < freq[max]; i++) {
+                sb.append((char) max);
+            }
+            freq[max] = 0;
+        }
+        return sb.toString();
+    }
+}

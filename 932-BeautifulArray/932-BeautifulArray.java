ee5a663e0f460eed485062e4fdfb227f9f738280
MUKILAN M <mukilanmuthiah15@gmail.com>
2026-09-07 16:28:44 +0530

    Create 932-BeautifulArray

---
+++ 932-BeautifulArray/932-BeautifulArray.java
@@ -0,0 +1,29 @@
+// Last updated: 9/7/2026, 4:28:44 PM
+class Solution {
+    public int[] beautifulArray(int n) {
+        int[] arr = new int[n];
+        arr[0] = 1;
+        int size = 1;
+        while (size < n) {
+            int[] temp = new int[n];
+            int index = 0;
+            for (int i = 0; i < size; i++) {
+                int value = 2 * arr[i] - 1;
+                if (value <= n) {
+                    temp[index] = value;
+                    index++;
+                }
+            }
+            for (int i = 0; i < size; i++) {
+                int value = 2 * arr[i];
+                if (value <= n) {
+                    temp[index] = value;
+                    index++;
+                }
+            }
+            arr = temp;
+            size = index;
+        }
+        return arr;
+    }
+}

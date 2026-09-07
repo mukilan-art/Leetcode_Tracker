09510b23cd66b295049273d7a6ce9d953e885a8c
MUKILAN M <mukilanmuthiah15@gmail.com>
2026-09-07 16:27:35 +0530

    Create 1295-FindNumbersWithEvenNumberOfDigits

---
+++ 1295-FindNumbersWithEvenNumberOfDigits/1295-FindNumbersWithEvenNumberOfDigits.java
@@ -0,0 +1,12 @@
+// Last updated: 9/7/2026, 4:27:34 PM
+class Solution {
+    public int findNumbers(int[] nums) {
+        int count = 0;
+        for(int i = 0;i<nums.length;i++){
+            if((nums[i]>=10 && nums[i]<=99) || (nums[i]>=1000 && nums[i]<=9999) || (nums[i] == 100000)){
+                count++;
+            }
+        }
+        return count;
+    }
+}

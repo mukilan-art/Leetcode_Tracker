07991d6da96c65258604ea0bce3dba2bcee85a03
MUKILAN M <mukilanmuthiah15@gmail.com>
2026-09-11 16:07:29 +0530

    Create 876-MiddleOfTheLinkedList

---
+++ 876-MiddleOfTheLinkedList/876-MiddleOfTheLinkedList.java
@@ -0,0 +1,14 @@
+// Last updated: 9/11/2026, 4:07:28 PM
+
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null)
+        {
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
+}

d5967157410e6159a20c5f940a0ff71c589cf638
MUKILAN M <mukilanmuthiah15@gmail.com>
2026-09-11 16:07:41 +0530

    Create 796-RotateString

---
+++ 796-RotateString/796-RotateString.java
@@ -0,0 +1,9 @@
+// Last updated: 9/11/2026, 4:07:40 PM
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if (s.length() != goal.length()) {
+            return false;
+        }
+        return (s + s).contains(goal);
+    }
+}

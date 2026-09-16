ddd88f5359d3230ad008acd82c6234375bedbc21
MUKILAN M <mukilanmuthiah15@gmail.com>
2026-09-16 16:25:59 +0530

    Create 1653-MinimumDeletionsToMakeStringBalanced

---
+++ 1653-MinimumDeletionsToMakeStringBalanced/1653-MinimumDeletionsToMakeStringBalanced.java
@@ -0,0 +1,14 @@
+// Last updated: 9/16/2026, 4:25:58 PM
+class Solution {
+    public int minimumDeletions(String s) {
+        int b_before_a = 0, deletion=0;
+        for(char ch: s.toCharArray()) {
+            if(ch == 'b') b_before_a+=1;
+            else if(b_before_a>0) {
+                b_before_a-=1;
+                deletion+=1;
+            }
+        }   
+        return deletion;
+    }
+}
